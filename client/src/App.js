7b59f6a76f142455fd589e8361ad0c3eb63709b2
Tomas Guignet <tomasguignet9@gmail.com>
2023-01-19 01:11:42 -0300

    Client- Routes created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {Routes , Route} from "react-router-dom";
+import index from "./components/Index/Index";
+import home from "./components/Home/Home";
+import RecipeDetail from "./components/RecipeDetail/RecipeDetail";
+import RecipeForm from "./components/RecipeForm/RecipeForm";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Routes>
+        <Route path="/" element={index} />
+        <Route path="/home" element={home}/>
+        <Route path="/recipes/:id" element={RecipeDetail}/>
+        <Route path="/recipeForm" element={RecipeForm}/>
+      </Routes>
     </div>
   );
 }
